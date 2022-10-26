bc10bfbf240145b5eeff7ee94a6903d1d8d2ae68
Rahul Ekbote <rahulek@gmail.com>
2022-10-26 16:55:46 +0530

    stringvars added.

---
+++ Basics/stringvars.js
@@ -0,0 +1,23 @@
+//Declate two string variables
+//let allows us to create and name the variables
+let fname = "John";
+let lname = "Doe";
+
+//display the vars
+console.log(fname);
+console.log(lname);
+
+//concat two strings together
+let fullName = fname + lname;
+console.log(fullName);
+
+//concat two strings together with a space between fname and lname
+fullName = fname + " " + lname;
+console.log(fullName);
+
+//Exercise
+// Define 2 string vars
+// One - username, Two - email
+// Declare and initialize
+// Output each with console.log
+// Concat both with a space between them
