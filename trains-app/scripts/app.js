1baf284fdd9c61e1645666e96e2bc1fbec226cc6
Rahul Ekbote <rahulek@gmail.com>
2022-10-30 16:47:11 +0530

    Create New elements and DOM insertion added.

--- trains-app/scripts/app.js
+++ trains-app/scripts/app.js
@@ -1,26 +1,26 @@
-//How to get to a <p> element on the page (index.html)
+//Creating elements on the fly
+//Inserting elements into the DOM
+
+//Create a <p> Element and append it to the existing list of trains
+//1. Create an element
+//2. Set its textContent
+//3. Insert it into DOM
 //
-//Older DOM API - document.getElementsByTagName()
-//Newer DOM API - document.querySelector()
+let pEl = document.createElement("p");
+pEl.textContent = "Train 546 Down";
+document.body.appendChild(pEl);
 
-const pElOlder = document.getElementsByTagName("p");
-console.log(pElOlder);
-
-//Newer DOM API - getSelector()
-//will return only the first <p>
-const pH3El = document.querySelector("h3");
-console.log(pH3El);
-//Delete it
-//pH3El.remove();
-
-//Access multiple <p>
-//print the textContent for each <p>
-const pElements = document.querySelectorAll("p");
-console.log(`number of elements: ${pElements.length}`);
-pElements.forEach(function (el) {
-  console.log(`${el.textContent}`);
+//Ex2 - adding multiple elements
+const trains = ["Gorakhpur Express", "Nizamuddin Express", "Rajdhani Express"];
+trains.forEach(function (train) {
+  const pEl = document.createElement("p");
+  pEl.textContent = train;
+  document.body.appendChild(pEl);
 });
 
-//Get the <h2> and change it to something else
-const pH2El = document.querySelector("h2");
-//pH2El.textContent = "Modified textContent via DOM API";
+//Exercise
+//    notes-app
+// 1. How will you get the body element alternatively using querySelector()
+// 2. Create a new <p> element and add it to the body
+// 3. Create an array of strings representing sample notes
+// 4. Insert all the notes into DOM as <p> elements
